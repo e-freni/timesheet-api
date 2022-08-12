5745318ca5da3185cd2212063fcf47862624538b
Enrico Freni <enrico@freni.it>
2022-08-12 13:00:28 +0200

    added get account info resource

--- src/main/java/com/jaewa/timesheet/controller/AccountController.java
+++ src/main/java/com/jaewa/timesheet/controller/AccountController.java
@@ -1,13 +1,17 @@
 package com.jaewa.timesheet.controller;
 
+import com.jaewa.timesheet.controller.dto.ApplicationUserDto;
 import com.jaewa.timesheet.controller.dto.LoginDto;
 import com.jaewa.timesheet.controller.dto.TokenDto;
+import com.jaewa.timesheet.controller.mapper.ApplicationUserMapper;
 import com.jaewa.timesheet.model.ApplicationUser;
 import com.jaewa.timesheet.service.ApplicationUserService;
+import com.jaewa.timesheet.service.AuthorizationService;
 import com.jaewa.timesheet.service.MailService;
 import com.jaewa.timesheet.service.token.TokenService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,28 +26,42 @@ public class AccountController {
     private final ApplicationUserService applicationUserService;
     private final MailService mailService;
     private final TokenService tokenService;
+    private final ApplicationUserMapper applicationUserMapper;
 
-    public AccountController(ApplicationUserService applicationUserService, MailService mailService, TokenService tokenService) {
+    public AccountController(ApplicationUserService applicationUserService, MailService mailService, TokenService tokenService, ApplicationUserMapper applicationUserMapper) {
         this.applicationUserService = applicationUserService;
         this.mailService = mailService;
         this.tokenService = tokenService;
+        this.applicationUserMapper = applicationUserMapper;
     }
+
+    @GetMapping("/account/info")
+    public ResponseEntity<ApplicationUserDto> getAccount() {
+
+        return applicationUserService.getById(AuthorizationService.getApplicationUserId())
+                .map(applicationUserMapper::toDTO)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+
+
 
     @PostMapping("/account/login")
     public ResponseEntity<TokenDto> login(@RequestBody LoginDto loginDto) {
-        return applicationUserService.getByLoginInfo(loginDto.getUsername())
-                .map(
-                        user -> {
-                            if (!applicationUserService.isValidPassword(user, loginDto.getPassword())) {
-                                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-                            }
-                            TokenDto tokenDto = TokenDto.builder()
-                                    .jwt(tokenService.createToken(user))
-                                    .build();
-                            return ResponseEntity.ok(tokenDto);
-                        }
-                )
-                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+        Optional<ApplicationUser> user = applicationUserService.getByLoginInfo(loginDto.getUsername());
+
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        if (!applicationUserService.isValidPassword(user.get(), loginDto.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        TokenDto tokenDto = TokenDto.builder()
+                .token(tokenService.createToken(user.get()))
+                .build();
+        return ResponseEntity.ok(tokenDto);
+
     }
 
     @PostMapping("/account/resetPassword")
--- src/main/java/com/jaewa/timesheet/controller/dto/TokenDto.java
+++ src/main/java/com/jaewa/timesheet/controller/dto/TokenDto.java
@@ -9,6 +9,6 @@ import lombok.Setter;
 @Builder
 public class TokenDto {
 
-    private String jwt;
+    private String token;
 
 }
